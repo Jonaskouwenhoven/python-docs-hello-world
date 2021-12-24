053c56e56b9ef6436a8f83b00aaa7cdd9986939a
Jonaskouwenhoven <33288237+Jonaskouwenhoven@users.noreply.github.com>
2021-12-24 13:34:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from sklearn.preprocessing import StandardScaler
+import time
+from sklearn import decomposition
+
+from sklearn.cluster import KMeans
+import shutil
+import math
+import os
+import argparse
+from skimage.transform import resize
+from skimage import img_as_bool
+from natsort import 
 from flask import Flask
 app = Flask(__name__)
 
